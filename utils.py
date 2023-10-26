2c610e721eb6637f31ab62bb1ac08ffd740145ba
leocraig18 <110471317+leocraig18@users.noreply.github.com>
2023-10-26 15:15:22 +0100

    Change dob function

--- utils.py
+++ utils.py
@@ -6,19 +6,24 @@ def create_people(num_of_iterations, cars) -> list:
     people = []
     for car in cars:
         for _ in range(num_of_iterations):
-            dob = generator.generate_random_birthdate()
-            postcode = generator.generate_random_postcode()
-            person_black = Person(name=generator.generate_random_name('Black'), dob=dob, postcode=postcode, car=car)
+            dob = generator.select_birthdate()
+            postcode_bw = generator.generate_random_postcode('BW')
+            postcode_aw = generator.generate_random_postcode('AW')
+            person_black = Person(name=generator.generate_random_name('Black'), dob=dob, postcode=postcode_bw[0], car=car)
             people.append(person_black)
-            person_white = Person(name=generator.generate_random_name('White'), dob=dob, postcode=postcode, car=car)
+            person_white = Person(name=generator.generate_random_name('White'), dob=dob, postcode=postcode_bw[1], car=car)
             people.append(person_white)
-            person_south_asian = Person(name=generator.generate_random_name('South_Asian'), dob=dob, postcode=postcode, car=car)
+            person_south_asian = Person(name=generator.generate_random_name('South_Asian'), dob=dob, postcode=postcode_aw[0], car=car)
             people.append(person_south_asian)
             print(person_black.__str__())
+            person_white2 = Person(name=generator.generate_random_name(
+                'White'), dob=dob, postcode=postcode_aw[1], car=car)
+            people.append(person_white2)
     return people
 
 def create_cars() -> list:
     cars = []
     car = Car("Honda", "Civic", "BF14MVM")
     cars.append(car)
+
     return cars
