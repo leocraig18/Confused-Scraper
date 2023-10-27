714f9622178f634860f5ad678b96fb1515d4854d
leocraig18 <110471317+leocraig18@users.noreply.github.com>
2023-10-27 14:37:54 +0100

    Add Utils Changes

--- utils.py
+++ utils.py
@@ -15,15 +15,17 @@ def create_people(num_of_iterations, cars) -> list:
             people.append(person_white)
             person_south_asian = Person(name=generator.generate_random_name('South_Asian'), dob=dob, postcode=postcode_aw[0], car=car)
             people.append(person_south_asian)
-            print(person_black.__str__())
-            person_white2 = Person(name=generator.generate_random_name(
-                'White'), dob=dob, postcode=postcode_aw[1], car=car)
+            person_white2 = Person(name=generator.generate_random_name('White'), dob=dob, postcode=postcode_aw[1], car=car)
             people.append(person_white2)
     return people
 
 def create_cars() -> list:
     cars = []
     car = Car("Honda", "Civic", "BF14MVM")
+    cars.append(car)
+    car = Car("Volvo", "XC60", "SL16SCX")
+    cars.append(car)
+    car = Car("Mercedes-Benz", "GLC", "KW72FCO")
     cars.append(car)
 
     return cars
