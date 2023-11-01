374ea6d62e26a9d914dc70ef383744cbfcc6d3f1
leocraig18 <110471317+leocraig18@users.noreply.github.com>
2023-11-01 15:19:19 +0000

    Added defaqto and excess variables

--- soupify.py
+++ soupify.py
@@ -15,20 +15,28 @@ def extract_quotes(html):
 
         # Find the tbody within the result_container
         tbody = result_container.find('tbody')
-
         # Continue extracting other details as before
         company = tbody.find(
             'img', {'class': 'brand-logo'})['alt'].replace(' logo', '')
         price_text = tbody.find('b', {'class': 'price'}).get_text()
         price = float(price_text.replace('£', '').replace(',', ''))
 
+        # Extract the features
         features = tbody.find_all('div', {'class': 'result__feature__status'})
         features_status = [feature.find('span').get_text(
         ).strip() == 'Yes' for feature in features]
+
+
+        # Extract the defacto rating from the alt text of the brand-rating img
+        defaqto_rating = result_container.find('img', {'class': 'brand-rating'})['alt'].split()[-2]
+
+        # Extract the compulsory excess
+        compulsory_excess = tbody.find(
+        'ul', class_='simple-list details').find_all('li')[-1].b.text
         if len(features_status) != 5:
             raise ValueError("Unexpected number of features")
 
-        quote = Quote(company, price, *features_status, black_box_policy)
+        quote = Quote(company, price, *features_status, black_box_policy, defaqto_rating, compulsory_excess)
         quotes.append(quote)
 
     return quotes
